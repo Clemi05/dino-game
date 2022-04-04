08eb19739a19d952191e2cb5fe862662f87d5859
Clement Azalbert <clementazalbert04@gmail.com>
2022-04-04 17:37:13 +0200

    added funct updateGround to ground.js + updated function in updateCustomProperty.js

--- js/ground.js
+++ js/ground.js
@@ -1,10 +1,10 @@
 import { incrementCustomProperty } from "./updateCustomProperty.js";
 
-
+const SPEED = 0.05;
 const groundElements = document.querySelectorAll('[data-ground]');
 
 export function updateGround(delta) {
   groundElements.forEach(ground => {
-    incrementCustomProperty()
+    incrementCustomProperty(ground, '--left', delta * SPEED * -1);
   })
 }
--- js/updateCustomProperty.js
+++ js/updateCustomProperty.js
@@ -3,10 +3,10 @@ export function getCustomProperty(elem, prop) {
   return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0
 }
 
-export function setCustomProperty(elem, prop) {
+export function setCustomProperty(elem, prop, value) {
   elem.style.setProperty(prop, value)
 }
 
-export function incrementCustomProperty(elem, prop) {
+export function incrementCustomProperty(elem, prop, inc) {
   setCustomProperty(elem, prop, getCustomProperty(elem, prop) + inc)
 }
