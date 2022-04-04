6fe715ff315f5d445ba55ca2c23f1589a4a75a24
Clement Azalbert <clementazalbert04@gmail.com>
2022-04-04 17:50:56 +0200

    added functs updateSpeedScale and handleStart in script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-import { updateGround } from "./ground.js";
+import { updateGround, setupGround } from "./ground.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
 
 const worldElem = document.querySelector('[data-world]');
 
-setPixelToWorldScale()
-window.addEventListener("resize", setPixelToWorldScale)
+setPixelToWorldScale();
+window.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener('keydown', handleStart, { once: true });
 
-let lastTime
+let lastTime;
+let speedScale;
 function update(time) {
   if (lastTime == null) {
     lastTime = time
@@ -17,13 +20,26 @@ function update(time) {
   }
   const delta = time - lastTime;
 
-  updateGround(delta)
+  updateGround(delta, speedScale);
+  updateSpeedScale(delta);
 
   lastTime = time
   window.requestAnimationFrame(update)
 }
 
-window.requestAnimationFrame(update)
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_SCALE_INCREASE;
+
+}
+
+function handleStart() {
+  lastTime = null;
+  speedScale = 1;
+  setupGround();
+  window.requestAnimationFrame(update);
+
+}
+
 
 function setPixelToWorldScale() {
   let worldToPixelScale;
