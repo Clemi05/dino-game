5dab3737a9d2fb0fb9eb69bd1d7e99ead038b950
Clement Azalbert <clementazalbert04@gmail.com>
2022-04-04 18:02:14 +0200

    added funct updateScore + hide title on script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
 const SPEED_SCALE_INCREASE = 0.00001;
 
-const worldElem = document.querySelector('[data-world]');
+const worldElement = document.querySelector('[data-world]');
+const scoreElement = document.querySelector('[data-score]');
+const startScreenElement = document.querySelector('[data-start-screen]');
 
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
@@ -12,6 +14,7 @@ document.addEventListener('keydown', handleStart, { once: true });
 
 let lastTime;
 let speedScale;
+let score;
 function update(time) {
   if (lastTime == null) {
     lastTime = time
@@ -22,6 +25,7 @@ function update(time) {
 
   updateGround(delta, speedScale);
   updateSpeedScale(delta);
+  updateScore(delta);
 
   lastTime = time
   window.requestAnimationFrame(update)
@@ -32,10 +36,17 @@ function updateSpeedScale(delta) {
 
 }
 
+function updateScore(delta) {
+  score += delta * 0.01;
+  scoreElement.textContent = Math.floor(score);
+}
+
 function handleStart() {
   lastTime = null;
   speedScale = 1;
+  score = 1;
   setupGround();
+  startScreenElement.classList.add("hide");
   window.requestAnimationFrame(update);
 
 }
@@ -49,6 +60,6 @@ function setPixelToWorldScale() {
     worldToPixelScale = window.innerHeight / WORLD_HEIGHT
   }
 
-  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
-  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
+  worldElement.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+  worldElement.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
 }
